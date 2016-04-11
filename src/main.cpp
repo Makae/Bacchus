6efcc2939f102c85bcb485cdc6ad7508b4937bed
makae <makae90@gmail.com>
2016-04-11 14:49:30 +0200

    Adding base structure

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <..\include\project\mouthtracker.h>
+
+using namespace std;
+
+
+int main(int argc, char** argv)
+{
+	Test();
+}
---
+++ src/mouthtracker.cpp
@@ -0,0 +1,8 @@
+#include <..\include\project\mouthtracker.h>
+#include <iostream>
+
+using namespace std;
+
+void Test(void) {
+	cout << "Test success";
+}
